f3fa587dcabe98c9484df0c88ecaa6780a96f07c
Samuel Mercado <samuelmer18@hotmail.com>
2023-03-01 13:13:29 -0400

    borrado de sidebar suscriptor

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -22,24 +22,7 @@
 	</div>
 </div>
 
-<div class="row">
-	<div class="box box-solid">
-	  	<div class="box-header with-border">
-	    	<h3 class="box-title"><b>Hazte suscriptor</b></h3>
-	  	</div>
-	  	<div class="box-body">
-	    <p>Obtenga actualizaciones gratuitas sobre los últimos productos y descuentos, directamente en su bandeja de entrada.</p>
-	    	<form method="POST" action="">
-		    	<div class="input-group">
-	                <input type="text" class="form-control">
-	                <span class="input-group-btn">
-	                    <button type="button" class="btn btn-info btn-flat"><i class="fa fa-envelope"></i> </button>
-	                </span>
-	            </div>
-		    </form>
-	  	</div>
-	</div>
-</div>
+
 
 <div class="row">
 	<div class='box box-solid'>
