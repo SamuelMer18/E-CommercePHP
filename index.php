58212c949d70bab9a4fcd92f129bc03c61cb03fa
Samuel Mercado <samuelmer18@hotmail.com>
2023-03-06 15:54:13 -0400

    trash img delete & new banners

--- index.php
+++ index.php
@@ -31,17 +31,13 @@
 		                <ol class="carousel-indicators">
 		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
 		                </ol>
 		                <div class="carousel-inner">
 		                  <div class="item active">
-		                    <img src="images/banner1.png" alt="First slide">
+		                    <img src="images/banner1.jpg" alt="First slide">
 		                  </div>
 		                  <div class="item">
-		                    <img src="images/banner2.png" alt="Second slide">
-		                  </div>
-		                  <div class="item">
-		                    <img src="images/banner3.png" alt="Third slide">
+		                    <img src="images/banner2.jpg" alt="Second slide">
 		                  </div>
 						</div>
 			
